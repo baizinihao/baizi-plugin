19cca7d5d11cbd0b63bca49796e112859fa84887
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-30 17:55:23 +0800

    细节优化

--- apps/广播.js
+++ apps/广播.js
@@ -1,14 +1,25 @@
-import plugin from '../../lib/plugins/plugin.js'
-import yaml from 'yaml'
-import { promises as fs } from 'fs'
-import common from '../../lib/common/common.js'
+import plugin from '../../../lib/plugins/plugin.js';
+import yaml from 'yaml';
+import { promises as fs } from 'fs';
+import path from 'path';
+import common from '../../../lib/common/common.js';
 
-//广播消息是否开启延迟 (默认为5秒)
-let delays = truefalse
-//发送消息延迟 (开启延迟后生效)
-let Nnumber = 5000
-//广播消息是否开启随机延迟 (需开启延迟后再开启随机延迟，默认在4到6秒内随机发送消息)
-let random_delays = truefalse
+// 配置文件路径：plugins/baizi-plugin/config/广播.json
+const configPath = path.join(process.cwd(), 'plugins', 'baizi-plugin', 'config', '广播.json');
+// 自动创建配置目录和文件
+if (!fs.existsSync(path.dirname(configPath))) {
+  fs.mkdirSync(path.dirname(configPath), { recursive: true });
+}
+if (!fs.existsSync(configPath)) {
+  const defaultConfig = {
+    delays: true,
+    Nnumber: 5000,
+    random_delays: false
+  };
+  fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2), 'utf8');
+}
+// 读取配置
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
 export class example2 extends plugin {
   constructor() {
@@ -23,65 +34,62 @@ export class example2 extends plugin {
           fnc: 'broadcast'
         }
       ]
-    })
+    });
   }
 
   async broadcast(e) {
     if (!e.isMaster) return true;
-    await e.reply(`请发送你要广播的内容`)
-    this.setContext('broadcast_')
+    await e.reply(`请发送你要广播的内容`);
+    this.setContext('broadcast_');
   }
 
   async broadcast_(e) {
-    this.finish('broadcast_')
-    let msg = e.msg.match(/^#(白名单|黑名单)?广播通知$/)
-    console.log(e.msg)
-    let otheryaml = await fs.readFile(`./config/config/other.yaml`, `utf-8`)
-    let other = yaml.parse(otheryaml)
-    if(!msg[1]){
-        let all_group = Array.from(Bot[e.self_id].gl.values())
-        let all_groupid = []
-        for (let item of all_group){
-            all_groupid.push(item.group_id)
-        }
-        await 发送消息(all_groupid, this.e.message, e)
-        e.reply(`广播已完成`)
-        return true
-    } else if(msg[1] == `白名单`){
-        if(other.whiteGroup.length == 0){
-            e.reply(`白名单为空，广播失败`)
-            return true;
-        }
-        await 发送消息(other.whiteGroup, this.e.message, e)
-        e.reply(`广播已完成`)
-        return true
-    } else if(msg[1] == `黑名单`){
-        if(other.blackGroup.length == 0){
-            e.reply(`黑名单为空，广播失败`)
-            return true;
-        }
-        await 发送消息(other.blackGroup, this.e.message, e)
-        e.reply(`广播已完成`)
-        return true
+    this.finish('broadcast_');
+    const msg = e.msg.match(/^#(白名单|黑名单)?广播通知$/);
+    console.log(e.msg);
+    // 正确拼接other.yaml路径
+    const otherYamlPath = path.join(process.cwd(), 'config', 'other.yaml');
+    const otheryaml = await fs.readFile(otherYamlPath, 'utf-8');
+    const other = yaml.parse(otheryaml);
+    let targetGroups = [];
+    if (!msg[1]) {
+      const all_group = Array.from(Bot[e.self_id].gl.values());
+      targetGroups = all_group.map(item => item.group_id);
+    } else if (msg[1] === '白名单') {
+      if (!other.whiteGroup || other.whiteGroup.length === 0) {
+        await e.reply(`白名单为空，广播失败`);
+        return true;
+      }
+      targetGroups = other.whiteGroup;
+    } else if (msg[1] === '黑名单') {
+      if (!other.blackGroup || other.blackGroup.length === 0) {
+        await e.reply(`黑名单为空，广播失败`);
+        return true;
+      }
+      targetGroups = other.blackGroup;
     }
+    await 发送消息(targetGroups, e.message, e);
+    await e.reply(`广播已完成`);
+    return true;
   }
 }
 
-async function 发送消息(group, message, e){
-    let groupNumber = group.length
-    for (let item of group) {
-        groupNumber--;
-        let number = 0
-        if(delays){
-            number = Nnumber
-        }
-        if(delays && random_delays){
-            number = Math.floor(Math.random() * (6000 - 4000 + 1)) + 4000;
-        }
-        await Bot[e.self_id].pickGroup(item).sendMsg(message)
-            .then(() => e.reply(`群${item}消息已送达，等待${number}毫秒后广播下一个群\n剩余${groupNumber}个群`))
-            .catch((err) => e.reply(`群${item}消息发送失败，等待${number}毫秒后广播下一个群\n剩余${groupNumber}个群\n错误码:${err.code}\n错误信息:${err.message}`))
-        await common.sleep(number)
+async function 发送消息(group, message, e) {
+  let groupNumber = group.length;
+  const { delays, Nnumber, random_delays } = config;
+  for (const item of group) {
+    groupNumber--;
+    let number = 0;
+    if (delays) {
+      number = Nnumber;
     }
-    return `OK`
+    if (delays && random_delays) {
+      number = Math.floor(Math.random() * (6000 - 4000 + 1)) + 4000;
+    }
+    await Bot[e.self_id].pickGroup(item).sendMsg(message)
+      .then(async () => await e.reply(`群${item}消息已送达，等待${number}毫秒后广播下一个群\n剩余${groupNumber}个群`))
+      .catch(async (err) => await e.reply(`群${item}消息发送失败，等待${number}毫秒后广播下一个群\n剩余${groupNumber}个群\n错误码:${err.code}\n错误信息:${err.message}`));
+    await common.sleep(number);
+  }
+  return `OK`;
 }
