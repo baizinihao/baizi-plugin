f5e68c8eafa9d9aab7f1cd4217ba11627847ab50
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-30 10:15:05 +0800

    细节优化

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ if (!global.segment) {
   global.segment = (await import("oicq")).segment
 }
 
+// 使用ANSI转义序列设置文本颜色
+logger.info('\x1b[33mbaizi插件载入成功！！！！！！\x1b[0m') // 橙色
+logger.info('\x1b[32m作者：baizi\x1b[0m') // 绿色
+logger.info('\x1b[33mQQ交流群：863644536\x1b[0m') // 橙色
+logger.info('\x1b[34m---------哼哼啊啊啊---------\x1b[0m') // 蓝色
 
-logger.info('\x1b[插件载入成功！！！！！！\x1b[0m')
-logger.info('\x1b[作者：baizi\x1b[0m')
-logger.info('\x1b[交流群：863644536\x1b[0m')
-logger.info('\x1b[34m---------哼哼啊啊啊---------\x1b[0m')
-
-
+// 动态加载插件
 async function loadPlugins() {
   const files = fs
-    .readdirSync('./plugins/baizi-plugin/apps')
+    .readdirSync('./plugins/jiuxian-plugin/apps')
     .filter(file => file.endsWith('.js'))
 
   let ret = files.map(file => import(`./apps/${file}`));
@@ -23,7 +23,7 @@ async function loadPlugins() {
   for (let i in files) {
     const name = files[i].replace('.js', '')
     if (ret[i].status !== 'fulfilled') {
-      logger.error(`\x1b[baizi-plugins载入插件错误：${name}\x1b[0m`)
+      logger.error(`\x1b[31m载入插件错误：${name}\x1b[0m`) // 红色
       logger.error(ret[i].reason)
       continue
     }
