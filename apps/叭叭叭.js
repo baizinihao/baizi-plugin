6316547204e6be55de2c94b853f5e328bf97fafb
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-30 19:43:23 +0800

    更新 叭叭叭.js

--- apps/叭叭叭.js
+++ apps/叭叭叭.js
@@ -1,10 +1,9 @@
-import plugin from '@/lib/plugins/plugin.js';
+import plugin from '../../../lib/plugins/plugin.js';
 
 export default class 光遇叫声 extends plugin {
   constructor() {
     super({
-      name: '光遇随机叫声',
-      dsc: '调用接口返回光遇叫声，支持多指令触发',
+      name: '光遇叫声',
       event: 'message',
       priority: 9999,
       rule: [
