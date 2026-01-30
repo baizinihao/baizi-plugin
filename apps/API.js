57eaf699febd79b4c62e306717c817de691a947b
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-31 04:55:15 +0800

    细节优化

---
+++ apps/API.js
@@ -0,0 +1,45 @@
+import plugin from '../../../lib/plugins/plugin.js';
+
+export class ApiQuery extends plugin {
+  constructor() {
+    super({
+      name: "API查询",
+      dsc: "更新日志|调用统计|延迟测试",
+      event: "message",
+      priority: 5000,
+      rule: [
+        { reg: "^#?更新日志$", fnc: "getLog" },
+        { reg: "^#?调用统计$", fnc: "getCount" },
+        { reg: "^#?延迟测试$", fnc: "getDelay" }
+      ]
+    });
+  }
+
+  async requestApi(url) {
+    try {
+      const controller = new AbortController();
+      const timeout = setTimeout(() => controller.abort(), 10000);
+      const res = await fetch(url, { signal: controller.signal });
+      clearTimeout(timeout);
+      if (!res.ok) throw new Error();
+      return await res.text();
+    } catch (e) {
+      return "获取失败，请稍后重试";
+    }
+  }
+
+  async getLog(e) {
+    const res = await this.requestApi("http://baizihaoxiao.xin/API/gn.php");
+    await e.reply(res);
+  }
+
+  async getCount(e) {
+    const res = await this.requestApi("http://baizihaoxiao.xin/API/ti.php");
+    await e.reply(res);
+  }
+
+  async getDelay(e) {
+    const res = await this.requestApi("http://baizihaoxiao.xin/API/ys.php");
+    await e.reply(res);
+  }
+}
