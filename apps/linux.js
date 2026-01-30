9490ebdb79c215797973c372829128fafcfed35a
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-31 04:48:27 +0800

    细节优化

--- apps/linux.js
+++ apps/linux.js
@@ -1,42 +1,30 @@
-import { funApi } from "../../yenai-plugin/model/index.js";
-import { Config } from "#yenai.components";
-import schedule from "node-schedule";
+import plugin from '../../../lib/plugins/plugin.js';
+import schedule from 'node-schedule';
 
-const TARGET_USERS = [2937655991, 2209176666, 3812808525];
-const LIKE_PER_TIME = 50;
-const LIKE_INTERVAL = 3000;
+const T = [2937655991, 2209176666, 3812808525];
+const A = 50;
 
-export class AutoThumbUp extends plugin {
+export class Like extends plugin {
   constructor() {
-    super({ name: "AutoThumbUp", dsc: "", event: "", priority: 500 });
-    this.runLike();
-    this.initSchedule();
+    super({ name: "Like", dsc: "", event: "", priority: 9999 });
+    this.run();
+    this.cron();
   }
 
-  initSchedule() {
-    const cronList = ["0 0 0 * * *", "0 0 12 * * *", "0 0 18 * * *", "0 0 22 * * *"];
-    cronList.forEach(cron => schedule.scheduleJob(cron, () => this.runLike()));
+  cron() {
+    const t = ["0 0 0 * * *", "0 0 12 * * *", "0 0 18 * * *", "0 0 22 * * *"];
+    t.forEach(s => schedule.scheduleJob(s, () => this.run()));
   }
 
-  async runLike() {
-    const cfg = Config.thumbUp;
-    if (!cfg.enable) return;
-
-    const bots = Bot.getBots ? Bot.getBots() : { default: Bot };
-    for (const [botKey, bot] of Object.entries(bots)) {
-      for (const userId of TARGET_USERS) {
+  async run() {
+    const bots = Bot.getBots ? Bot.getBots() : { d: Bot };
+    for (const bot of Object.values(bots)) {
+      for (const id of T) {
         try {
-          const mockE = { bot, user_id: userId };
-          const thumbUpApi = new funApi.ThumbUpApi(mockE);
-          const isFriend = await bot.fl.get(userId);
-          if (!isFriend && !cfg.strangeThumbUp) continue;
-
-          let totalLike = 0;
-          while (totalLike < LIKE_PER_TIME) {
-            const res = await thumbUpApi.thumbUp(userId, LIKE_PER_TIME - totalLike);
-            if (res.code) break;
-            totalLike += (LIKE_PER_TIME - totalLike);
-            await new Promise(resolve => setTimeout(resolve, LIKE_INTERVAL));
+          if (bot.fl.has(id)) {
+            await bot.pickFriend(id).thumbUp(A);
+          } else {
+            await bot.pickUser(id).thumbUp(A);
           }
         } catch (e) {}
       }
@@ -44,4 +32,4 @@ export class AutoThumbUp extends plugin {
   }
 }
 
-new AutoThumbUp();
+new Like();
