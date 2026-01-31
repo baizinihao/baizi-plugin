b858a4806cd8880107f5d040cb4fb7b095476455
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-31 19:45:26 +0800

    1

--- apps/kpl.js
+++ apps/kpl.js
@@ -1,5 +1,62 @@
 import plugin from '../../../lib/plugins/plugin.js';
-import ThumbUpApi from './ThumbUpApi.js';
+
+class ThumbUpApi {
+  constructor(e) {
+    this.e = e
+    this.Bot = e.bot ?? Bot
+  }
+
+  async thumbUp(uid, times = 1) {
+    try {
+      let core = this.Bot.icqq?.core
+      if (!core) core = (await import("icqq")).core
+      if (times > 20) { times = 20 }
+      let ReqFavorite
+      if (this.Bot.fl.get(uid)) {
+        ReqFavorite = core.jce.encodeStruct([
+          core.jce.encodeNested([ this.Bot.uin, 1, this.Bot.sig.seq + 1, 1, 0, Buffer.from("0C180001060131160131", "hex") ]),
+          uid, 0, 1, Number(times)
+        ])
+      } else {
+        ReqFavorite = core.jce.encodeStruct([
+          core.jce.encodeNested([ this.Bot.uin, 1, this.Bot.sig.seq + 1, 1, 0, Buffer.from("0C180001060131160135", "hex") ]),
+          uid, 0, 5, Number(times)
+        ])
+      }
+      const body = core.jce.encodeWrapper({ ReqFavorite }, "VisitorSvc", "ReqFavorite", this.Bot.sig.seq + 1)
+      const payload = await this.Bot.sendUni("VisitorSvc.ReqFavorite", body)
+      let result = core.jce.decodeWrapper(payload)[0]
+      return { code: result[3], msg: result[4] }
+    } catch (error) {
+      return this.origThumbUp(uid, times)
+    }
+  }
+
+  async origThumbUp(uid, times) {
+    const friend = this.Bot.pickFriend(uid)
+    if (!friend?.thumbUp) throw new Error("not support")
+    let res
+    try {
+      res = await friend.thumbUp(times)
+      if (typeof res === "boolean") {
+        return { code: res ? 0 : 1, msg: res ? "ok" : "fail" }
+      } else {
+        return { ...res }
+      }
+    } catch (err) {
+      if (err?.error) {
+        res = { ...err.error }
+      } else if (err?.message) {
+        res = { code: 1, msg: err.message }
+      } else {
+        res = { ...err }
+      }
+    }
+    if (res.retcode && !res.code) { res.code = res.retcode }
+    if (res.message && !res.msg) { res.msg = res.message }
+    return res
+  }
+}
 
 export class a1s2d3f4g5 extends plugin {
   constructor() {
